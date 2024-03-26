cbaa408b4294745f3bcb1e2bf2c2094f91d15a42
leonardogabriels <leonardogabrielsilva1@outlook.com>
2024-03-25 22:54:27 -0300

    consulta com @query

--- src/main/java/com/leonardo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/leonardo/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,15 @@ package com.leonardo.workshopmongo.repository;
 import com.leonardo.workshopmongo.entity.Post;
 import com.leonardo.workshopmongo.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
+    @Query("{ 'title': {$regex : ?0, $options: 'i'}}")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
